bd83a6a368f3fab08cf7cdbd38e3832af86e127d
worai <42282410+worai@users.noreply.github.com>
2022-11-12 12:26:32 +0100

    Sound options & Mixer control
    Master volume, music volume, effects volume;
    Snapshots (2), Expose variables (r-click variable for mixer control slider thing)
    First test-track in sample scene

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
 
+  [SerializeField] private AudioMixer mixer;
+  [SerializeField] private AudioSource src;
+
+  [SerializeField] private AudioMixerSnapshot maxSnapShot;
+  [SerializeField] private AudioMixerSnapshot minSnapShot;
+
   public Sound[] sounds;
 
   private void Awake()
@@ -15,6 +21,20 @@ public class AudioManager : MonoBehaviour
       s.source.volume = s.volume;
       s.source.pitch = s.pitch;
     }
+
+  }
+
+  private void Start()
+  {
+    // when this guy is in Start, and not in Awake, then it is respected
+    // TODO fix audio
+    //  - make it set the volume before the first frame
+    //  - OR don't play anything on the first frame
+    //  - OR make the music softer on the first frame... <- prob not an option
+    //  - OR just make it volue zero from the start!
+    if(!mixer.SetFloat("UserMusicVolume", -80f)) Debug.LogError("not found");
+    //src.enabled = true;
+    mixer.TransitionToSnapshots(new AudioMixerSnapshot[] { maxSnapShot }, new float[] { 1f }, 1f);
   }
 
 }
--- Assets/Scripts/UI/Menus/OptionsMenu.cs
+++ Assets/Scripts/UI/Menus/OptionsMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 /// <summary>
 /// https://www.youtube.com/watch?v=JRnNqQ2wbOU
@@ -10,10 +11,19 @@ public class OptionsMenu : MonoBehaviour
   List<int> widths = new List<int> { 640, 800, 1024 };
   List<int> heights = new List<int> { 480, 600, 768 };
 
+  [SerializeField] private AudioMixer mainMixer;
 
-  public void SetVolume(float value)
+  public void SetMainVolume(float value)
   {
-    //TODO AudioMixer or something
+    mainMixer.SetFloat("UserMasterVolume", value);
+  }
+  public void SetMusicVolume(float value)
+  {
+    mainMixer.SetFloat("UserMusicVolume", value);
+  }
+  public void SetEffectsVolume(float value)
+  {
+    mainMixer.SetFloat("UserEffectsVolume", value);
   }
 
   public void SetSCreenSize (int index)
